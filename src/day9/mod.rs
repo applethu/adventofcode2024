368da6e3cc62a0d4edc4d2651c054fa8ca1bc7f6
tz <tz@x.local>
2024-12-09 19:53:06 +0100

    day 9 part 2

--- src/day9/mod.rs
+++ src/day9/mod.rs
@@ -16,30 +16,66 @@ pub fn run() {
         }
     }
 
+    // PART 1
+    // while data.len() > 0 {
+    //     let current = data.remove(0);
+    //     for _ in 0..current.0 {
+    //         result.push(current.1);
+    //     }
+    //     let freespace = space.remove(0);
+    //     if data.len() == 0 {
+    //         break;
+    //     }
+    //     if freespace == 0 {
+    //         continue;
+    //     }
+    //     let last = data.pop().unwrap();
+    //     if last.0 <= freespace {
+    //         data.insert(0, (last.0, last.1));
+    //         let remainingspace = freespace - last.0;
+    //         space.insert(0, remainingspace);
+    //     } else if freespace > 0 {
+    //         let overflow = last.0 - freespace;
+    //         data.push((overflow, last.1));
+    //         data.insert(0, (freespace, last.1));
+    //         space.insert(0, 0);
+    //     }
+    // }
+
+    // PART 2
+    let mut handled_index = data.len() - 1;
+    while handled_index > 1 {
+        let current = data.remove(handled_index);
+        let mut handled_space_index = 0;
+        while handled_space_index < handled_index && space[handled_space_index] < current.0 {
+            handled_space_index += 1;
+        }
+        if handled_space_index == handled_index {
+            data.insert(handled_index, current);
+            handled_index -= 1;
+        } else {
+            data.insert(handled_space_index+1, current);
+            space[handled_space_index] -= current.0;
+            space[handled_index-1] += current.0;
+            space.insert(handled_space_index,0);
+            if handled_index < space.len() -1 {
+                space[handled_index] += space[handled_index+1];
+                space.remove(handled_index+1);
+            }
+        }
+    }
+
     while data.len() > 0 {
         let current = data.remove(0);
         for _ in 0..current.0 {
             result.push(current.1);
         }
         let freespace = space.remove(0);
-        if data.len() == 0 {
-            break;
-        }
-        if freespace == 0 {
-            continue;
-        }
-        let last = data.pop().unwrap();
-        if last.0 <= freespace {
-            data.insert(0, (last.0, last.1));
-            let remainingspace = freespace - last.0;
-            space.insert(0, remainingspace);
-        } else if freespace > 0 {
-            let overflow = last.0 - freespace;
-            data.push((overflow, last.1));
-            data.insert(0, (freespace, last.1));
-            space.insert(0, 0);
+        for _ in 0..freespace {
+            result.push(0);
         }
     }
+
     let mut checksum = 0;
     for i in 0..result.len() {
         checksum += result[i] * i as u64;
